f13e5354c293eb341ca4babb99b6bfb5359efbd2
h5mcbox <h503mc@gmail.com>
2024-06-16 00:37:54 +0800

    build:Update submodule and remove core.lib

--- src/core.lib/src/services/NodeIKernelNodeMiscService.d.ts
+++
@@ -1,5 +0,0 @@
-import { GeneralCallResult } from "./common";
-export interface NodeIKernelNodeMiscService {
-    wantWinScreenOCR(imagepath: string): Promise<GeneralCallResult>;
-    SendMiniAppMsg(arg1: string, arg2: string, arg3: string): unknown;
-}
